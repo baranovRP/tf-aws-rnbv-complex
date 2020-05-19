e23364f95ae85f40d6fe141490a9fa754f71b276
baranovrp <baranov.r.p@gmail.com>
2020-05-19 12:14:20 +0200

    TFAWS-06 Create tf module to communicate with aws secretmanager

--- live/dev/webserver-cluster/variables.tf
+++ live/dev/webserver-cluster/variables.tf
@@ -1,3 +1,10 @@
 locals {
-  instance_type = "t2.micro"
+  instance_type     = "t2.micro"
+  secretmanager_key = "dev/atlantis/github"
+  alb_dns_name      = "http://${module.alb.alb_dns_name}/events"
+  webhook_secret    = module.security.webhook_sectret
+  github_username   = module.security.github_username
+  github_token      = module.security.github_token
+  github_repo       = "tf-aws-rnbv-complex"
+  github_repo_url   = "github.com/${local.github_username}/${local.github_repo}"
 }
